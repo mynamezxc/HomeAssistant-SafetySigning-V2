bf7bb59368ebeea6c4ba3771449d33ab5c5086a4
Nguyên Hoàng <35897654+mynamezxc@users.noreply.github.com>
2022-03-01 10:48:50 +0700

    -

--- custom_components/safety_signing/config_flow.py
+++ custom_components/safety_signing/config_flow.py
@@ -25,11 +25,9 @@ _LOGGER = logging.getLogger(__name__)
 # quite work as documented and always gave me the "Lokalise key references" string
 # (in square brackets), rather than the actual translated value. I did not attempt to
 # figure this out or look further into it.
-DATA_SCHEMA = vol.Schema([{
-    ("host"): str
-}, {
+DATA_SCHEMA = vol.Schema({
     ("name"): str
-}]),
+})
 
 
 
