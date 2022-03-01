f2fc7d9d0c26cf8cd9e83cc3575b290eed0ad310
Nguyên Hoàng <35897654+mynamezxc@users.noreply.github.com>
2022-03-01 10:46:44 +0700

    -

--- custom_components/safety_signing/config_flow.py
+++ custom_components/safety_signing/config_flow.py
@@ -25,13 +25,12 @@ _LOGGER = logging.getLogger(__name__)
 # quite work as documented and always gave me the "Lokalise key references" string
 # (in square brackets), rather than the actual translated value. I did not attempt to
 # figure this out or look further into it.
-DATA_SCHEMA = [vol.Schema({
-        ("host"): str
-    }),
-    vol.Schema({
-        ("name"): str
-    })
-]
+DATA_SCHEMA = vol.Schema([{
+    ("host"): str
+}, {
+    ("name"): str
+}]),
+
 
 
 async def validate_input(hass: HomeAssistant, data: dict) -> dict[str, Any]:
