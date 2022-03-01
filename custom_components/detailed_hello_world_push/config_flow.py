13ea3e1eebb7e920d2fea589ed9fb83bfc32cadc
Nguyên Hoàng <35897654+mynamezxc@users.noreply.github.com>
2022-03-01 09:11:34 +0700

    Update field

--- custom_components/detailed_hello_world_push/config_flow.py
+++ custom_components/detailed_hello_world_push/config_flow.py
@@ -25,47 +25,27 @@ _LOGGER = logging.getLogger(__name__)
 # quite work as documented and always gave me the "Lokalise key references" string
 # (in square brackets), rather than the actual translated value. I did not attempt to
 # figure this out or look further into it.
-DATA_SCHEMA = vol.Schema({("host"): str})
-
+DATA_SCHEMA = vol.Schema(
+    {
+        "name": str,
+        "access_token": dict,
+        "token_serial": str,
+        "serial_number": str,
+        "pin": str
+    }
+)
 
 async def validate_input(hass: HomeAssistant, data: dict) -> dict[str, Any]:
     """Validate the user input allows us to connect.
 
     Data has the keys from DATA_SCHEMA with values provided by the user.
     """
-    # Validate the data can be used to set up a connection.
+    if len(data["token_serial"]) < 5 or len(data["serial_number"]) <= 5:
+        raise InvalidSerial
+    if 5 >= len(data["pin"]) >= 8:
+        raise InvalidPin
 
-    # This is a simple example to show an error in the UI for a short hostname
-    # The exceptions are defined at the end of this file, and are used in the
-    # `async_step_user` method below.
-    if len(data["host"]) < 3:
-        raise InvalidHost
-
-    hub = Hub(hass, data["host"])
-    # The dummy hub provides a `test_connection` method to ensure it's working
-    # as expected
-    result = await hub.test_connection()
-    if not result:
-        # If there is an error, raise an exception to notify HA that there was a
-        # problem. The UI will also show there was a problem
-        raise CannotConnect
-
-    # If your PyPI package is not built with async, pass your methods
-    # to the executor:
-    # await hass.async_add_executor_job(
-    #     your_validate_func, data["username"], data["password"]
-    # )
-
-    # If you cannot connect:
-    # throw CannotConnect
-    # If the authentication is wrong:
-    # InvalidAuth
-
-    # Return info that you want to store in the config entry.
-    # "Title" is what is displayed to the user for this hub device
-    # It is stored internally in HA as part of the device config.
-    # See `async_step_user` below for how this is used
-    return {"title": data["host"]}
+    return {"title": data["name"]}
 
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -92,14 +72,15 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 info = await validate_input(self.hass, user_input)
 
                 return self.async_create_entry(title=info["title"], data=user_input)
-            except CannotConnect:
-                errors["base"] = "cannot_connect"
-            except InvalidHost:
+            except InvalidSerial:
                 # The error string is set here, and should be translated.
                 # This example does not currently cover translations, see the
                 # comments on `DATA_SCHEMA` for further details.
                 # Set the error on the `host` field, not the entire form.
-                errors["host"] = "cannot_connect"
+                errors["token_serial"] = "Invalid token or serial number"
+                errors["serial_number"] = "Invalid token or serial number"
+            except InvalidPin:
+                errors["pin"] = "Invalid length of pin code"
             except Exception:  # pylint: disable=broad-except
                 _LOGGER.exception("Unexpected exception")
                 errors["base"] = "unknown"
@@ -113,6 +94,9 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 class CannotConnect(exceptions.HomeAssistantError):
     """Error to indicate we cannot connect."""
 
+class InvalidSerial(exceptions.HomeAssistantError):
+    """Error to indicate there is an invalid length of serial."""
 
-class InvalidHost(exceptions.HomeAssistantError):
-    """Error to indicate there is an invalid hostname."""
+
+class InvalidPin(exceptions.HomeHomeAssistantError):
+    """Error to indicate there is an invalid pin code."""
--- custom_components/detailed_hello_world_push/const.py
+++ custom_components/detailed_hello_world_push/const.py
@@ -2,4 +2,4 @@
 
 # This is the internal name of the integration, it should also match the directory
 # name for the integration.
-DOMAIN = "detailed_hello_world_push"
+DOMAIN = "safety_signing"
