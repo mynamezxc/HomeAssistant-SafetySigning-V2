086b925355bc21d6fe2db04d90cbeb64c648dd19
Nguyên Hoàng <35897654+mynamezxc@users.noreply.github.com>
2022-03-04 09:16:11 +0700

    -

--- custom_components/safety_signing/cover.py
+++ custom_components/safety_signing/cover.py
@@ -27,12 +27,12 @@ async def async_setup_entry(
     async_add_entities: AddEntitiesCallback,
 ) -> None:
     """Add cover for passed config_entry in HA."""
-    # The token is loaded from the associated hass.data entry that was created in the
+    # The hub is loaded from the associated hass.data entry that was created in the
     # __init__.async_setup_entry function
-    token = hass.data[DOMAIN][config_entry.entry_id]
+    hub = hass.data[DOMAIN][config_entry.entry_id]
 
     # Add all entities to HA
-    async_add_entities(HelloWorldCover(cron) for cron in token.crons)
+    async_add_entities(HelloWorldCover(roller) for roller in hub.rollers)
 
 
 # This entire class could be written to extend a base class to ensure common attributes
@@ -49,26 +49,22 @@ class HelloWorldCover(CoverEntity):
     # device it connected to), then this should be function with an @property decorator.
     supported_features = SUPPORT_SET_POSITION | SUPPORT_OPEN | SUPPORT_CLOSE
 
-    def __init__(self, cron) -> None:
+    def __init__(self, roller) -> None:
         """Initialize the sensor."""
         # Usual setup is done here. Callbacks are added in async_added_to_hass.
-        self._cron = cron
+        self._roller = roller
 
         # A unique_id for this entity with in this domain. This means for example if you
         # have a sensor on this cover, you must ensure the value returned is unique,
         # which is done here by appending "_cover". For more information, see:
         # https://developers.home-assistant.io/docs/entity_registry_index/#unique-id-requirements
         # Note: This is NOT used to generate the user visible Entity ID used in automations.
-        self._attr_unique_id = f"{self._cron.cron_id}_cover"
+        self._attr_unique_id = f"{self._roller.roller_id}_cover"
 
         # This is the name for this *entity*, the "name" attribute from "device_info"
         # is used as the device name for device screens in the UI. This name is used on
         # entity screens, and used to build the Entity ID that's used is automations etc.
-        self._attr_name = self._cron.name
-        self._attr_token_serial = self._cron.token_serial
-        self._attr_serial_number = self._cron.serial_number
-        self._attr_pin = self._cron.pin
-        self._attr_access_token = self._cron.access_token
+        self._attr_name = self._roller.name
 
     async def async_added_to_hass(self) -> None:
         """Run when this Entity has been added to HA."""
@@ -78,12 +74,12 @@ class HelloWorldCover(CoverEntity):
         # called where ever there are changes.
         # The call back registration is done once this entity is registered with HA
         # (rather than in the __init__)
-        self._cron.register_callback(self.async_write_ha_state)
+        self._roller.register_callback(self.async_write_ha_state)
 
     async def async_will_remove_from_hass(self) -> None:
         """Entity being removed from hass."""
         # The opposite of async_added_to_hass. Remove any registered call backs here.
-        self._cron.remove_callback(self.async_write_ha_state)
+        self._roller.remove_callback(self.async_write_ha_state)
 
     # Information about the devices that is partially visible in the UI.
     # The most critical thing here is to give this entity a name so it is displayed
@@ -108,23 +104,23 @@ class HelloWorldCover(CoverEntity):
     def device_info(self) -> DeviceInfo:
         """Information about this entity/device."""
         return {
-            "identifiers": {(DOMAIN, self._cron.cron_id)},
+            "identifiers": {(DOMAIN, self._roller.roller_id)},
             # If desired, the name for the device could be different to the entity
             "name": self.name,
-            "sw_version": self._cron.firmware_version,
-            "model": self._cron.model,
-            "manufacturer": self._cron.token.manufacturer,
+            "sw_version": self._roller.firmware_version,
+            "model": self._roller.model,
+            "manufacturer": self._roller.hub.manufacturer,
         }
 
     # This property is important to let HA know if this entity is online or not.
     # If an entity is offline (return False), the UI will refelect this.
     @property
     def available(self) -> bool:
-        """Return True if cron and token is available."""
-        return self._cron.online and self._cron.token.online
+        """Return True if roller and hub is available."""
+        return self._roller.online and self._roller.hub.online
 
     # The following properties are how HA knows the current state of the device.
-    # These must return a value from memory, not make a live query to the device/token
+    # These must return a value from memory, not make a live query to the device/hub
     # etc when called (hence they are properties). For a push based integration,
     # HA is notified of changes via the async_write_ha_state call. See the __init__
     # method for hos this is implemented in this example.
@@ -134,33 +130,33 @@ class HelloWorldCover(CoverEntity):
     @property
     def current_cover_position(self):
         """Return the current position of the cover."""
-        return self._cron.position
+        return self._roller.position
 
     @property
     def is_closed(self) -> bool:
         """Return if the cover is closed, same as position 0."""
-        return self._cron.position == 0
+        return self._roller.position == 0
 
     @property
     def is_closing(self) -> bool:
         """Return if the cover is closing or not."""
-        return self._cron.moving < 0
+        return self._roller.moving < 0
 
     @property
     def is_opening(self) -> bool:
         """Return if the cover is opening or not."""
-        return self._cron.moving > 0
+        return self._roller.moving > 0
 
     # These methods allow HA to tell the actual device what to do. In this case, move
     # the cover to the desired position, or open and close it all the way.
     async def async_open_cover(self, **kwargs: Any) -> None:
         """Open the cover."""
-        await self._cron.set_position(100)
+        await self._roller.set_position(100)
 
     async def async_close_cover(self, **kwargs: Any) -> None:
         """Close the cover."""
-        await self._cron.set_position(0)
+        await self._roller.set_position(0)
 
     async def async_set_cover_position(self, **kwargs: Any) -> None:
         """Close the cover."""
-        await self._cron.set_position(kwargs[ATTR_POSITION])
+        await self._roller.set_position(kwargs[ATTR_POSITION])
