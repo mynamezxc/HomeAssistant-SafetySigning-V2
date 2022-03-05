aee9c43c690b5566ac4ee477b5f935381f66d35b
Nguyên Hoàng <35897654+mynamezxc@users.noreply.github.com>
2022-03-05 20:45:19 +0700

    -

--- custom_components/safety_signing/token.py
+++ custom_components/safety_signing/token.py
@@ -126,7 +126,7 @@ class Crons:
             }
         }
         requestURL = API_URL + "/autoSign"
-        response = requests.post(requestURL, data=json.dumps(requestBody), headers=requestHeaders)
+        response = await requests.post(requestURL, data=json.dumps(requestBody), headers=requestHeaders)
         if response:
             response = response.json()
             if "status" not in response or response["status"] != 0:
