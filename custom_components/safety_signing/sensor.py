4bfb61362dab7ecb53bd475c0f11e4354c9a77fc
Nguyên Hoàng <35897654+mynamezxc@users.noreply.github.com>
2022-03-07 16:03:22 +0700

    -\

--- custom_components/safety_signing/sensor.py
+++ custom_components/safety_signing/sensor.py
@@ -13,8 +13,9 @@ from homeassistant.const import (
     DEVICE_CLASS_ILLUMINANCE,
     PERCENTAGE,
 )
+from homeassistant.components.device_automation.const import CONF_IS_OFF, CONF_IS_ON
 from homeassistant.helpers.entity import Entity
-
+from homeassistant.components.binary_sensor import BinarySensorEntity
 from .const import DOMAIN
 
 
@@ -41,7 +42,7 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
 # This base class shows the common properties and methods for a sensor as used in this
 # example. See each sensor for further details about properties and methods that
 # have been overridden.
-class SensorBase(Entity):
+class SensorBase(BinarySensorEntity):
     """Base representation of a Hello World Sensor."""
 
     should_poll = False
@@ -72,13 +73,13 @@ class BatterySensor(SensorBase):
     # The class of this device. Note the value should come from the homeassistant.const
     # module. More information on the available devices classes can be seen here:
     # https://developers.home-assistant.io/docs/core/entity/sensor
-    device_class = DEVICE_CLASS_BATTERY
+    device_class = "running"
 
     # The unit of measurement for this entity. As it's a DEVICE_CLASS_BATTERY, this
     # should be PERCENTAGE. A number of units are supported by HA, for some
     # examples, see:
     # https://developers.home-assistant.io/docs/core/entity/sensor#available-device-classes
-    _attr_unit_of_measurement = "running"
+    # _attr_unit_of_measurement = "running"
 
     def __init__(self, cron):
         """Initialize the sensor."""
@@ -95,14 +96,6 @@ class BatterySensor(SensorBase):
         """Icon of the entity."""
         return "mdi:alarm-light"
 
-    def toggle(self):
-        """Return the state of the sensor."""
-        self._cron.toggle_cron()
-
-    @property
-    async def async_toggle(self):
-        """Return the state of the sensor."""
-        self._cron.toggle_cron()
 
     # The value of this sensor. As this is a DEVICE_CLASS_BATTERY, this value must be
     # the battery level as a percentage (between 0 and 100)
@@ -111,7 +104,10 @@ class BatterySensor(SensorBase):
         """Return the state of the sensor."""
         return self._cron.is_running
 
-
+    @property
+    def is_on(self):
+        """Return true if the binary sensor is on."""
+        return self._state
 
 # This is another sensor, but more simple compared to the battery above. See the
 # comments above for how each field works.
