59e0071df1c19f7f50ae602ffe1865de9f8f98fb
Nguyên Hoàng <35897654+mynamezxc@users.noreply.github.com>
2022-03-07 14:28:51 +0700

    -

--- custom_components/safety_signing/__init__.py
+++ custom_components/safety_signing/__init__.py
@@ -28,12 +28,12 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     # hass.config_entries.async_setup_platforms(entry, PLATFORMS)
     hass.async_create_task(
         hass.config_entries.async_forward_entry_setup(
-            entry, PLATFORMS[0]
+            entry, "sensor"
         )
     )
     hass.async_create_task(
         hass.config_entries.async_forward_entry_setup(
-            entry, PLATFORMS[1]
+            entry, "cover"
         )
     )
     return True
