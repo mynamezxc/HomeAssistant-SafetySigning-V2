0d28e0ac489348faecec75bc91bedd9635f1ac8a
Nguyên Hoàng <35897654+mynamezxc@users.noreply.github.com>
2022-03-07 14:20:04 +0700

    -

--- custom_components/safety_signing/sensor.py
+++ custom_components/safety_signing/sensor.py
@@ -30,9 +30,11 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
     token = hass.data[DOMAIN][config_entry.entry_id]
 
     new_devices = []
-    for cron in token.crons:
-        new_devices.append(BatterySensor(cron))
-        # new_devices.append(IlluminanceSensor(cron))
+    if not hass.data[DOMAIN][config_entry.entry_id].installed():
+        for cron in token.crons:
+            new_devices.append(BatterySensor(cron))
+            # new_devices.append(IlluminanceSensor(cron))
+            # hass.data[DOMAIN][config_entry.entry_id].set_installed()
     if new_devices:
         async_add_entities(new_devices)
 
