cd7828c36ccbcbca679da990790bab9f89aad6fc
Nguyên Hoàng <35897654+mynamezxc@users.noreply.github.com>
2022-03-07 15:47:45 +0700

    -

--- custom_components/safety_signing/const.py
+++ custom_components/safety_signing/const.py
@@ -3,4 +3,4 @@
 # This is the internal name of the integration, it should also match the directory
 # name for the integration.
 DOMAIN = "safety_signing"
-API_URL = "http://172.16.14.89:3000/api"
+API_URL = "http://192.168.11.66:3000/api"
